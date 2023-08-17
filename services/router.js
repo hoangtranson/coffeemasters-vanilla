6f432236b9a93147d2759482c1cc80ca881e93c7
hoangtranson <son.hoang01@gmail.com>
2023-08-17 22:07:53 +0700

    Enhance Links for SPA

--- services/router.js
+++ services/router.js
@@ -1,6 +1,14 @@
 const Router = {
     init: () => {
-        
+        document.querySelectorAll("a.navlink").forEach(a => {
+            a.addEventListener("click", event => {
+                event.preventDefault();
+                const href = event.target.getAttribute("href");
+                Router.go(href);
+            });
+        });  
+        // Process initial URL   
+        Router.go(location.pathname);
     },    
     go: (route, addToHistory=true) => {
 
