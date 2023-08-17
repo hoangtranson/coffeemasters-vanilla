f276f29d7845c96a793a7a8f0183e89175a5bd5c
hoangtranson <son.hoang01@gmail.com>
2023-08-17 22:00:06 +0700

    Event Binding for Initialization

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
 HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
 HTMLElement.prototype.$ = (s) => this.querySelector(s);
 HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
+
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM is ready");
+});
