402a5b598c224cf895b7a51a53d8f638cc076171
hoangtranson <son.hoang01@gmail.com>
2023-08-17 22:09:58 +0700

    Reacting to URLs

--- services/router.js
+++ services/router.js
@@ -11,7 +11,33 @@ const Router = {
         Router.go(location.pathname);
     },    
     go: (route, addToHistory=true) => {
-
+        if (addToHistory) {
+            history.pushState({ route }, '', route);
+        }
+        let pageElement = null;
+        switch (route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent ="Menu";
+                break;
+            default:
+                if (route.startsWith("/product-")) {                
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Details";
+                    pageElement.dataset.productId = route.substring(route.lastIndexOf("-")+1);
+                }
+                break;   
+        }
+        if (pageElement) {
+            document.querySelector("main").innerHTML = "";
+            document.querySelector("main").appendChild(pageElement);
+        }
+    
+        window.scrollX = 0;
     }
 }
 
