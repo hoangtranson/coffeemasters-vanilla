337b559a4f11b774d09c651dad8bd773fde27225
hoangtranson <son.hoang01@gmail.com>
2023-08-17 23:05:50 +0700

    Listening for Cart Updates

--- app.js
+++ app.js
@@ -25,4 +25,9 @@ window.addEventListener("DOMContentLoaded", () => {
     loadData();
 });
 
-
+window.addEventListener("appcartchange", event => {
+    const badge = document.getElementById("badge");
+    const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+    badge.textContent = qty;
+    badge.hidden = qty == 0;
+});
