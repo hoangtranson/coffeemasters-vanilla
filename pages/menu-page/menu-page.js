0725a336fad14602ca207940340721f254d92673
hoangtranson <son.hoang01@gmail.com>
2023-08-17 22:46:27 +0700

    Rendering Menu

--- pages/menu-page/menu-page.js
+++ pages/menu-page/menu-page.js
@@ -19,6 +19,34 @@ export default class MenuPage extends HTMLElement {
         loadCSS();
     }
 
+    connectedCallback() {
+        const template = document.getElementById("menu-page-template");
+        const content = template.content.cloneNode(true);
+        this.root.appendChild(content);
+
+        window.addEventListener("appmenuchange", () => {
+            this.render();
+        });
+        this.render();
+    }
+
+    render() {
+        if (app.store.menu) {
+            this.root.querySelector("#menu").innerHTML = "";
+            for (let category of app.store.menu) {
+                const liCategory = document.createElement("li");
+                liCategory.innerHTML = `
+                    <h3>${category.name}</h3>
+                    <ul class='category'>
+                    </ul>`;
+                this.root.querySelector("#menu").appendChild(liCategory);
+    
+                // TODO: placeholder
+            }  
+        } else {
+            this.root.querySelector("#menu").innerHTML = `Loading...`;
+        }
+    }
     
 }
 customElements.define("menu-page", MenuPage);
